9826ed2d6e2e619410561192253cce9aaf80b490
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-04-06 16:06:44 +0900

    feat: Add bar button and desciption button
    Removes the default back button, registers a custom button.
    And adds a function to display full description when the detailed button is pressed.

---
+++ bobmongus/bobmongus/RoomPageView.swift
@@ -0,0 +1,51 @@
+//
+//  RoomPageView.swift
+//  bobmongus
+//
+//  Created by Hyeonsoo Kim on 2022/04/06.
+//
+
+import SwiftUI
+
+struct RoomPageView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var backBtn: some View {
+        Button {
+            self.presentationMode.wrappedValue.dismiss() //추후 ActionSheet로.
+        } label: {
+            Text("< 나가기")
+        }
+    }
+    
+    var description: String = "포항공대 근처에서 아주 유명한 맛집인 xxxx에서 주문하려합니다!! 배달요금은 4000원이고, C5건물앞에 모여서 수령하면 좋을 것 같아요!! 모두 레디하시면 오카방 링크가 뜬다고하니 들어와주시면 감사하겠습니다!! ㅎㅎ"
+    
+    @State private var isClick: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button {
+                self.isClick.toggle()
+            } label: {
+                isClick ?
+                Text(description) :
+                Text("상세설명")
+                    .foregroundColor(.purple)
+            }
+        }
+        .navigationTitle("title") //추후
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                backBtn
+            }
+        }
+    }
+}
+
+struct RoomPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomPageView()
+    }
+}
