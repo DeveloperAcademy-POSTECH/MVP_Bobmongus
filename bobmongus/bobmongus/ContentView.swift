e1c027e88b0f8d833b78aed277211be17d025637
100seo <deepgreen7362@gmail.com>
2022-04-12 00:45:34 +0900

    style: popup detail design

--- bobmongus/bobmongus/ContentView.swift
+++ bobmongus/bobmongus/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 //방만들기 버튼
+                Image("background-iPhone 13")
                 Button(action: {
                     withAnimation {
                         self.showModal.toggle()
@@ -27,29 +28,28 @@ struct ContentView: View {
                     }
                 }
                 //방만들기 버튼 끝
+                
                 //모달 창 시작
                 if showModal {
-                    //검은배경처리
+                    //모달창 백그라운드
                     Rectangle()
                         .foregroundColor(Color.black.opacity(0.6))
                         .edgesIgnoringSafeArea(.all)
-                    //모달창 배경
+                    //모달창
                     ZStack {
                         RoundedRectangle(cornerRadius: 16)
-                            .padding(.horizontal)
-                            .frame(width: 390.0, height: 330.0)
-                            .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.917))
+                            .frame(width: 350.0, height: 340.0)
+                            .foregroundColor(Color.white)
                             .overlay(ModalContentView(showModal: self.$showModal))
                     }
-                    .transition(.move(edge: .bottom))
+                    //.transition(.move(edge: .bottom))
                 }
                 //모달창 배경 끝
             }
-            //문제1 네비게이션으로 작업해서 모달처리해도 타이틀은 고정됨
-            //안토니가 네비게이션 타이틀로 했을 시 그부분은 투명도(모달처리) 안됨
+            //네비게이션 타이틀은 투명도(모달처리) 안됨
             .navigationBarTitle("room list back")
         }
-        //문제2 기능 확인
+        //한 번에 하나의 상위 뷰만 표시하는 뷰 스택으로 표시되는 탐색 뷰 스타일
         .navigationViewStyle(StackNavigationViewStyle())
     }
     //네비게이션 뷰 끝
@@ -62,44 +62,48 @@ struct ModalContentView: View {
     //초기값 현재시간으로 설정
     @State private var currentDate = Date()
     
+    
     var body: some View {
         //모달창 내부 시작
         VStack(alignment: .leading) {
-            Spacer()
-            
-            Button(action: {
+            Button {
                 withAnimation {
                     self.showModal.toggle()
                 }
-            }) {
+            } label: {
                 HStack {
                     Image(systemName: "xmark.circle.fill")
-                        .imageScale(.large)
+                        .imageScale(.medium)
+                        .foregroundColor(Color.red)
                     Text("창 닫기")
-                }
+                        .font(.custom("DungGeunMo", size: 17))
+                }.foregroundColor(Color.black)
             }
-            .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
-            
-            TextField("방제목", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            .padding(.vertical, 10.0)
+
+            TextField("방 제목", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                 .textFieldStyle(.roundedBorder)
-            TextField("세부설명", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("세부 설명 (만남 위치, 배달비 등등)", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                 .textFieldStyle(.roundedBorder)
             TextField("카카오톡 오픈채팅 주소", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                 .textFieldStyle(.roundedBorder)
             
             Stepper(value: $sleepAmount, in: 2...6) {
                 Image(systemName: "person.fill")
-                Text("\(sleepAmount)명 (본인포함)")
+                Text("\(sleepAmount)명(본인포함)")
+                    .font(.custom("DungGeunMo", size: 17))
             }
+            .padding(.top, 10.0)
             
             DatePicker(selection: $currentDate, displayedComponents: .hourAndMinute, label: {
                 Image(systemName: "timer")
                 Text("마감시간설정")
+                    .font(.custom("DungGeunMo", size: 17))
             })
+            //모달창 내부 끝
             
             
-            //이버튼을 방 내부와 연결하면 될 듯
-            //문제3 버튼 정렬만 센터로 따로 설정하는법 현재 VStack으로 왼쪽정렬로 고정함
+            //방만들기 버튼
             VStack(alignment: .center) {
                 Button(action: {
                     withAnimation {
@@ -108,19 +112,24 @@ struct ModalContentView: View {
                 }) {
                     HStack {
                         Image(systemName: "fork.knife")
-                            .imageScale(.large)
+                            .imageScale(.medium)
+                            
                         Text("방 만들기")
-                            .fontWeight(.bold)
-                        
+                            .font(.custom("DungGeunMo", size: 20))
                     }.padding(10.0)
                         .background(
                             RoundedRectangle(cornerRadius: 10)
-                                .stroke(lineWidth: 2)
+                                //.stroke(lineWidth: 2)
+                                .shadow(color:.black, radius: 0, x:2 ,y: 3)
+
+                                .foregroundColor(Color(red: 0.6352941176470588, green: 0.396078431372549, blue: 0.7372549019607844))
+                               
                         )
                 }
-                .foregroundColor(.purple)
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
                 Spacer().frame(maxWidth:.infinity)
             }
+            .padding(.top, 5.0)
             //Vstack기준 10개 넘기면 에러 Extra argument in call
         }.padding(30)
     }
